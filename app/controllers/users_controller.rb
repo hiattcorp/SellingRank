e36d2e0d4277b32c09bb54f4a801a77c57e34ab8
Tim Park <timsjpark@gmail.com>
2016-04-06 21:15:35 -0600

    Remove unnecessary whitespace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       redirect_to root_path,
         notice: "Thank you for signing up #{@user.first_name.capitalize}"
