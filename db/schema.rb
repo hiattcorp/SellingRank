26fa9e45b2562720bd26678125f9f7fc35d23ba2
Jerin Hiatt <jerinhiatt@gmail.com>
2016-04-13 19:03:21 -0600

    Minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403134450) do
+ActiveRecord::Schema.define(version: 20160409203658) do
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "category"
-    t.string   "question_text"
-    t.string   "option1"
-    t.string   "option2"
-    t.string   "option3"
-    t.string   "option4"
-    t.string   "option5"
-    t.string   "answer"
-    t.string   "points"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+  create_table "survey_answers", force: :cascade do |t|
+    t.integer  "attempt_id"
+    t.integer  "question_id"
+    t.integer  "option_id"
+    t.boolean  "correct"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "survey_attempts", force: :cascade do |t|
+    t.integer "participant_id"
+    t.string  "participant_type"
+    t.integer "survey_id"
+    t.boolean "winner"
+    t.integer "score"
+  end
+
+  create_table "survey_options", force: :cascade do |t|
+    t.integer  "question_id"
+    t.integer  "weight",      default: 0
+    t.string   "text"
+    t.boolean  "correct"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "survey_questions", force: :cascade do |t|
+    t.integer  "survey_id"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "survey_surveys", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "attempts_number", default: 0
+    t.boolean  "finished",        default: false
+    t.boolean  "active",          default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
