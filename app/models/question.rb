3da99dc7c91b662f66b069cdffe4d6cc63ecefa6
Tim Park <timsjpark@gmail.com>
2016-04-18 21:15:53 -0600

    Fix nested_attributes methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :test
+  
   has_many :options, dependent: :destroy
-  accepts_nested_attributes_for :options,
-    allow_destroy:  true
+
+  accepts_nested_attributes_for :options, allow_destroy: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,5 @@
 class Test < ActiveRecord::Base
   has_many :questions, dependent: :destroy
 
-  accepts_nested_attributes_for :questions,
-    allow_destroy: true,
-    reject_if: proc { |attributes| attributes['name'].blank? }
+  accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: :all_blank
 end
