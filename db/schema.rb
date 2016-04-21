1eb4aa8f99d85588b3b386acfdd763d1a76f462d
Tim Park <timsjpark@gmail.com>
2016-04-20 19:43:54 -0600

    Add user_id to attempts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421010342) do
+ActiveRecord::Schema.define(version: 20160421014313) do
 
   create_table "attempts", force: :cascade do |t|
     t.integer  "test_id"
     t.integer  "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "options", force: :cascade do |t|
