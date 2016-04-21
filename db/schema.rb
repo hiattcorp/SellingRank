72feeba88ad0445754c1578c4bce11479a7c1073
Tim Park <timsjpark@gmail.com>
2016-04-20 19:10:58 -0600

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417162730) do
+ActiveRecord::Schema.define(version: 20160421010342) do
+
+  create_table "attempts", force: :cascade do |t|
+    t.integer  "test_id"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "options", force: :cascade do |t|
     t.integer  "question_id"
