dc17f1f493740164e0c9ac1869136fe8444b6d39
Tim Park <timsjpark@gmail.com>
2016-04-28 19:55:53 -0600

    Configure UserMailer and create views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@sellingrank.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to Selling Rank!')
+  end
 end
