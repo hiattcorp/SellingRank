c136042d4002fa2db6b65b0c506d530fb23fab07
Tim Park <timsjpark@gmail.com>
2016-04-28 20:33:30 -0600

    TODO - Fix some Mailer settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ApplicationMailer
+  # TODO - Fix the default 'from' address
   default from: 'no-reply@sellingrank.com'
 
   def welcome_email(user)
     @user = user
+    # TODO - Actually link the profile page
     @url = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to Selling Rank!')
   end
