ca550fa50faeb193bd55ccc61379b48cbd931b20
Jerin Hiatt <jerinhiatt@gmail.com>
2016-04-29 13:49:43 -0600

    leaderboard update

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20160428025620) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_tests", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
