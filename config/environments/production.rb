bcd6d11c4d05e5ee8cbcee960f7240afffb44ee4
Tim Park <timsjpark@gmail.com>
2016-04-29 23:12:13 -0600

    Fixed da mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://sellingrank.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.net',
+    address: 'smtp.gmail.com',
     port: '587',
     domain: 'gmail.com',
     user_name: ENV['GMAIL_USERNAME'],
