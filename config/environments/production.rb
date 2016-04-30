42cebf482dfd636090580b0bd69af9df09fd1025
Jerin Hiatt <jerinhiatt@gmail.com>
2016-04-29 21:18:11 -0600

    Configure production settings to use gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://sellingrank.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
+    address: 'smtp.gmail.net',
     port: '587',
     domain: 'heroku.com',
     authentication: 'plain',
     enable_starttls_auto: true,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
